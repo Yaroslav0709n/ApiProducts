50e525985364e57a247cb58481a3b25476a08e36
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-03-22 17:27:09 +0200

    Add files via upload

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiProducts.Data
+{
+    public class ApiDbContext:DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+        public DbSet<ProductItem> Products { get; set; }
+    }
+}
